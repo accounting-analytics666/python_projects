66e45ce542b684a7097484b8bff625f782c8a4bd
Celine <celinexz@hotmail.com>
2024-10-16 10:33:57 -0400

    first draft

---
+++ Downloading Financial Statements by Ticker.py
@@ -0,0 +1,47 @@
+import requests
+import pandas as pd
+import numpy as np
+import openpyxl
+
+# Replace YOUREMAIL@DOMAINNAME.com with your own email
+
+headers = {'User-Agent': 'YOUREMAIL@DOMAINNAME.com'}
+
+# Get all companies' CIKs
+
+ciks = requests.get("https://www.sec.gov/files/company_tickers.json", headers = headers)
+ciks_all = pd.DataFrame(ciks.json()).T
+ciks_all['cik_str']=ciks_all['cik_str'].astype(str).str.zfill(10)
+
+# Request input for the ticker symbol of a company
+
+ticker = str(input("Please enter the company's ticker:"))
+cik = ciks_all[ciks_all['ticker'] == ticker]['cik_str'][0]
+
+# Get facts
+
+facts = requests.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers = headers)
+tags = facts.json()['facts']['us-gaap'].keys()
+
+# Compile all downloaded facts into a DataFrame
+
+data_all = pd.DataFrame()
+data = pd.DataFrame()
+dummykey = {}
+key_dic = {}
+for tag in tags:
+    concepts = requests.get(f'https://data.sec.gov/api/xbrl/companyconcept/CIK{cik}/us-gaap/{tag}.json', headers = headers)
+    condition = concepts.json()['units'].keys()
+    for i in condition:
+        dummykey[i]= i
+        key_dic.update(dummykey)
+        unit = key_dic.keys()
+        for j in unit:
+            if j in condition:
+                data_all = pd.DataFrame(concepts.json()['units'][j])
+                data_all['acct'] = tag
+                data_all['unit'] = j
+                data = pd.concat([data, data_all],join = 'outer') 
+
+# Save to an Excel file
+data.to_csv('secfilings.csv')
---
+++ README.md
@@ -0,0 +1,11 @@
+## This code snippet allow you to download SEC filings for US public companies using their ticker symbol.
+
+** It was tested on Python 3.12 with a completion time of less than 2 minutes.**
+
+## The following procedures may be followed to use the program:
+
+1. Create a Python (or equivalent) file and save it in a folder.
+2. Copy and paste the code snippet into the Python file.
+3. Replace "YOUREMAIL@DOMANINNAME.com" with your actual email address.
+4. Run the code. 
+5. The output will be exported to the folder where your Python file is saved, in csv format, as "secfilings.csv".
